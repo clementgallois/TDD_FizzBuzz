77ab27f41a1a5722258a959cf79f4b89d551c1bd
clement.gallois@epitech.eu <clement.gallois@epitech.eu>
2020-10-30 14:25:31 +0100

    Refactor: Add constants

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
  // A number is fizz if it is divisible by 3 or if it has a 3 in it
  // A number is buzz if it is divisible by 5 or if it has a 5 in it
+
+const FIZZ_VALUE = 3;
+const FIZZ_STRING = "Fizz";
+const BUZZ_VALUE = 5;
+const BUZZ_STRING = "Buzz";
+
 function fizzBuzz(num){
     let result = '';
-    if (num % 3 === 0) {
-        result += 'Fizz';
+    if (num % FIZZ_VALUE === 0) {
+        result += FIZZ_STRING;
     }
-    if (num % 5 === 0) {
-        result += 'Buzz';
+    if (num % BUZZ_VALUE === 0) {
+        result += BUZZ_STRING;
     }
     return result || num;
 }
