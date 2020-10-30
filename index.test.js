b2fc5301c53ea1438ba42afcda4e1d67a99d6031
clement.gallois@epitech.eu <clement.gallois@epitech.eu>
2020-10-30 14:04:21 +0100

    fizzBuzz with 2 returns 2

--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ const fizzBuzz = require('./index.js');
 test('fizzBuzz(1) returns 1', () => {
   expect(fizzBuzz(1)).toBe(1);
 });
+
+test('fizzBuzz(2) returns 2', () => {
+  expect(fizzBuzz(2)).toBe(2);
+});
